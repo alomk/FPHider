21d99fa21646bf8d4d978af56a8245b4fd04f7f6
Kazi <alomk@mit.edu>
2019-05-12 22:47:18 -0400

    user-agent spoofing works now

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
 var current_user_agent = "Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0"
+var checked = false
 
 function rewriteUserAgentHeader(e) {
+  if (!checked){
+     return;
+  }
   e.requestHeaders.forEach(function(header){
     if (header.name.toLowerCase() == "user-agent") {
-      header.value = default_user_agent;
+      header.value = current_user_agent;
     }
   });
   return {requestHeaders: e.requestHeaders};
@@ -17,5 +21,6 @@ browser.webRequest.onBeforeSendHeaders.addListener(
 );
 
 function change_user_agent(s){
+    console.log("hit");
     current_user_agent = s
 }
--- popup/choose_action.js
+++ popup/choose_action.js
@@ -1,13 +1,17 @@
 var background = browser.extension.getBackgroundPage();
+user_agent_input = document.getElementById('uatext')
 
-var user_agent_input = document.getElementById('uatext');
 user_agent_input.value = background.current_user_agent;
 
+document.getElementById('check_ua').checked = background.checked
+
 document.addEventListener("click", (e) => {
-  if (!e.target.classList.contains("ua-choice")) {
+  if (!e.target.classList.contains("check")) {
     return;
   }
 
-  var chosen = e.target.textContent;
-  background.setUaString(chosenUa);
+
+  var chosen = user_agent_input.value;
+  background.checked = !background.checked
+  background.change_user_agent(chosen);
 });
